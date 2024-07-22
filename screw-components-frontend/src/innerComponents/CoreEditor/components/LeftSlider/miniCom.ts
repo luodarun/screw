f9df21cb587f9600c917cfbfe1317d8cbc03ab5f
run.luo <run.luo@17zuoye.com>
2024-07-22 20:44:43 +0800

    fix

--- screw-components-frontend/src/innerComponents/CoreEditor/components/LeftSlider/miniCom.ts
+++ screw-components-frontend/src/innerComponents/CoreEditor/components/LeftSlider/miniCom.ts
@@ -12,9 +12,9 @@ export default defineComponent({
   setup(props) {
     const divEl = ref();
     return () => h('div', { ref: divEl }, [
-      h('span', {}, props.info?.name),
       props.info?.icon && h(Icon, {icon: props.info.icon, ...props.info?.iconStyle}),
-      props.info?.img && h('img', {src: props.info.img})
+      props.info?.img && h('img', {src: props.info.img}),
+      h('span', {}, props.info?.name),
     ]);
   }
 })
